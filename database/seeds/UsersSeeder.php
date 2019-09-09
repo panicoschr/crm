93012068b52d5b9602cdd3701d1282d8470f3a35
Christodoulou <Christodoulou@DESKTOP-G7DLDR4.mshome.net>
2019-09-09 15:31:41 +0300

    changed the db to mysql. use seeder to insert 1st user admin in db

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+    factory(App\User::class, 1)->create([
+        'email' => 'admin@admin.com',
+        'password' => 'password',
+        ]);
+    }
+}
